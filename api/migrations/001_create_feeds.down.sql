d9b5bf1e35e66df031527f59e5d580c27a64b9f1
patrickhaahr <git@haahr.me>
2026-01-17 01:58:43 +0100

    chore(api): add feeds database migration

---
+++ api/migrations/001_create_feeds.down.sql
@@ -0,0 +1,2 @@
+DROP TABLE IF EXISTS feed_items;
+DROP TABLE IF EXISTS feeds;
---
+++ api/migrations/001_create_feeds.up.sql
@@ -0,0 +1,15 @@
+CREATE TABLE feeds (
+    id INTEGER PRIMARY KEY,
+    url TEXT UNIQUE NOT NULL,
+    enabled BOOLEAN DEFAULT TRUE
+);
+CREATE TABLE feed_items (
+    id INTEGER PRIMARY KEY,
+    feed_id INTEGER NOT NULL REFERENCES feeds(id) ON DELETE CASCADE,
+    title TEXT NOT NULL,
+    description TEXT,
+    link TEXT NOT NULL,
+    guid TEXT NOT NULL,
+    pub_date DATETIME,
+    UNIQUE(feed_id, guid)
+);
