9b9519544905eb47447fde65c2c8555c5431ac39
patrickhaahr <git@haahr.me>
2026-01-29 23:50:29 +0100

    chore(build): load VAULT_PASSWORD from .env at compile time for stronghold

--- mobile/src-tauri/build.rs
+++ mobile/src-tauri/build.rs
@@ -1,3 +1,15 @@
 fn main() {
+    // Load .env file to get VAULT_PASSWORD
+    if let Err(e) = dotenvy::from_path("../.env") {
+        println!("cargo:warning=Could not load .env file: {}", e);
+    }
+
+    // Pass VAULT_PASSWORD to the compiler so std::env! can see it
+    if let Ok(password) = std::env::var("VAULT_PASSWORD") {
+        println!("cargo:rustc-env=VAULT_PASSWORD={}", password);
+    } else {
+        panic!("VAULT_PASSWORD must be set in .env file for vault compilation");
+    }
+
     tauri_build::build()
 }
