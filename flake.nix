e3c7529c9b1ed02f99b1652468e83000298f31f3
patrickhaahr <git@haahr.me>
2026-05-11 22:40:20 +0200

    feat(mobile): add android-cli in flake and create android native project

--- flake.nix
+++ flake.nix
@@ -12,13 +12,47 @@
         pkgs = import nixpkgs {
           inherit system;
         };
+
+        android-cli = pkgs.stdenvNoCC.mkDerivation {
+          pname = "android-cli";
+          version = "latest";
+
+          src = pkgs.fetchurl {
+            url = "https://dl.google.com/android/cli/latest/linux_x86_64/android-cli";
+            sha256 = "sha256-pX5XLaxz2ziLsa6UKrWt5gb8a9psHskA1X43Zd5DY0A=";
+          };
+
+          dontUnpack = true;
+          nativeBuildInputs = [
+            pkgs.autoPatchelfHook
+            pkgs.makeWrapper
+          ];
+          buildInputs = [
+            pkgs.glibc
+          ];
+
+          installPhase = ''
+            runHook preInstall
+            mkdir -p $out/bin
+            mkdir -p $out/libexec
+            cp $src $out/libexec/android
+            chmod +x $out/libexec/android
+            makeWrapper $out/libexec/android $out/bin/android \
+              --set-default JAVA_TOOL_OPTIONS "-Djdk.lang.Process.launchMechanism=VFORK" \
+              --add-flags "--no-metrics"
+            runHook postInstall
+          '';
+        };
       in {
+        packages.android-cli = android-cli;
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
+            android-cli
             cargo
             cargo-watch
             clippy
-	    just
+            just
             openssl
             pkg-config
             rust-analyzer
---
+++ mobile-native/app/src/main/java/com/example/openhome/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.openhome
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.openhome.theme.OpenhomeTheme
+
+class MainActivity : ComponentActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+
+    enableEdgeToEdge()
+    setContent {
+      OpenhomeTheme { Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) { MainNavigation() } }
+    }
+  }
+}
